a01b12d26cb6548d334b32428439adafd0a61d47
Leo Schick <leonhard.schick@gmail.com>
2022-05-22 08:42:20 +0200

    fix public key in InternalsVisibleTo assembly attribute

--- src/Kapok.DataPort/AssemblyInfo.cs
+++ src/Kapok.DataPort/AssemblyInfo.cs
@@ -1,3 +1,12 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Kapok.DataPort.UnitTest, fb21470c71926024")] 
+[assembly: InternalsVisibleTo("Kapok.DataPort.UnitTest, PublicKey=00240000048000009400000006020000"
+                                                                          + "00240000525341310004000001000100"
+                                                                          + "1D89F2BC950A6A06EA7D316A11758517"
+                                                                          + "3ED4E19949494EA880BD34A8B2CEA5A2"
+                                                                          + "397410AB655E6FFADE3B3993E3C831EB"
+                                                                          + "88BE93EB0AB98C40500F84FABE3890A4"
+                                                                          + "D156B53BA8C30AA7A4B761A8371E0C25"
+                                                                          + "68AF7609678D4D2590F690045584801D"
+                                                                          + "81256E808DD0BD7687DFA1FEB394D5DB"
+                                                                          + "02113CC87DB59EC3F3B203C0766B35A7")]
