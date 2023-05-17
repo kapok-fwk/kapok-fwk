9e02af136d6d1e255cd36416bc8e2928489731a1
Leo Schick <leonhard.schick@gmail.com>
2023-05-17 11:25:00 +0200

    drop unused private field

--- src/Kapok.DataPort/Csv/CsvDataPortTarget.cs
+++ src/Kapok.DataPort/Csv/CsvDataPortTarget.cs
@@ -4,7 +4,6 @@ public class CsvDataPortTarget : IDataPortTableTarget
 {
     private CsvHelper.LineSeparator _columnSeparator = CsvHelper.LineSeparator.Comma;
     private bool _headerIsWritten;
-    private char _quoteChar;
 
     public virtual string Name => "CSV Data Port";
 
