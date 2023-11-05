038f730fd75b86fc077eba5d4aa9b4345abbab82
Leo Schick <leonhard.schick@gmail.com>
2023-11-05 14:11:03 +0100

    add dependency injection for ReportEngine

---
+++ src/Kapok.Report.UnitTest/ReportEngineTest.cs
@@ -0,0 +1,53 @@
+﻿using Kapok.Data.EntityFrameworkCore.UnitTest;
+using Kapok.Module;
+using Xunit;
+
+namespace Kapok.Report.UnitTest;
+
+public class TestExcelReportSheet : ExcelReport
+{
+    public TestExcelReportSheet()
+    {
+        Name = "Sheet_1";
+        this.Caption = "Sheet 1";
+    }
+}
+
+public class TextExcelReport : ExcelReportPackage
+{
+    public TextExcelReport()
+    {
+        Name = nameof(TextExcelReport);
+
+        SubReports.Add(new TestExcelReportSheet());
+    }
+}
+
+public class ReportEngineTest : DeferredDaoTestBase
+{
+    protected override void InitiateModule()
+    {
+        ModuleEngine.InitiateModule(typeof(Kapok.Report.ReportModule));
+        ExcelReportPackageProcessor.Register();
+    }
+
+    [Fact]
+    public void TestEmptyExcelReport()
+    {
+        var reportEngine = new ReportEngine(DataDomain);
+        var report = new TextExcelReport();
+
+        var memoryStream = new MemoryStream();
+        var streamWriter = new StreamWriter(memoryStream);
+
+        reportEngine.ExecuteReport(report, new Dictionary<string, object>(), ExcelReportPackageProcessor.XlsxMimeType, streamWriter.BaseStream);
+
+        memoryStream.Seek(0, SeekOrigin.Begin);
+
+        var streamReader = new StreamReader(memoryStream);
+
+        var result = streamReader.ReadToEnd();
+        Assert.NotNull(result);
+        Assert.NotEqual(string.Empty, result);
+    }
+}
